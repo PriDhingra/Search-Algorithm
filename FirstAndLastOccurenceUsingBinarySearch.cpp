b5b1a68644b1f3bc5552d679f0d53e945da39441
PriDhingra <princedhingra25@gmail.com>
2022-01-21 13:06:08 -0500

    Create FirstAndLastOccurenceUsingBinarySearch.cpp

---
+++ FirstAndLastOccurenceUsingBinarySearch.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int firstOcc(int arr[], int n, int key) {
+    int start = 0;
+    int end  = n - 1;
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+    
+    while(start <= end) {
+        if(arr[mid] == key) {
+            ans = mid;
+            end = mid - 1;
+        } else if(arr[mid] > key) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    
+    return ans;
+}
+
+int lastOcc(int arr[], int n, int key) {
+    int start = 0;
+    int end  = n - 1;
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+    
+    while(start <= end) {
+        if(arr[mid] == key) {
+            ans = mid;
+            start = mid + 1;
+        } else if(arr[mid] > key) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    
+    return ans;
+}
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 3, 5};
+   
+    cout<<"First Occurence of 3 is at "<<firstOcc(arr, 5, 3)<<" index"<<"\n";
+    cout<<"Last Occurence of 3 is at "<<lastOcc(arr, 5, 3)<<" index";
+   
+   return 0;
+}
