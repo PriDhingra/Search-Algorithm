ce193f8a89756a97ee497399eee46c88e75300d1
PriDhingra <princedhingra25@gmail.com>
2022-03-03 00:07:41 -0500

    Update PeakElementUsingBinarySearch.cpp

--- PeakElementUsingBinarySearch.cpp
+++ PeakElementUsingBinarySearch.cpp
@@ -1,28 +1,24 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-int peakEl(int arr[], int n) {
-    int start = 0;
-    int end = n - 1;
-    int mid = start + (end - start) / 2;
-    
+int peakElement(int *arr, int start, int end) {
+    int mid = start + ((end - start) / 2);
     while(start < end) {
-        if(arr[mid] < arr[mid + 1]) {
+        if(arr[mid] < arr[mid+1]) {
             start = mid + 1;
         } else {
             end = mid;
         }
-        mid = start + (end - start) / 2;
+        mid = start + ((end - start) / 2);
     }
     return start;
 }
 
-int main()
-{
-    int arr[] = {0, 10, 11, 18, 16, 7, 3, 1, 0};
-    
-    cout<<peakEl(arr, 9);
-   
+int main() {
+    int arr[] = {3, 4, 5, 1};
+
+    cout<<"Peak Element "<<arr[peakElement(arr, 0, 3)];
+
     return 0;
 }
